d5f386567120a329942cb7502499c44f8c850f2b
JavierBoscan <javierboscan16@gmail.com>
2020-12-10 22:11:11 -0400

    agregando middlewares y configurada peticion GET en para la api

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,34 @@
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
+	require('dotenv').config();
 }
 console.log(process.env.NODE_ENV);
 
 const cors = require('cors'),
-    express = require('express'),
-    morgan = require('morgan'),
-    multer = require('multer'),
-    path = require('path');
+	express = require('express'),
+	morgan = require('morgan'),
+	multer = require('multer'),
+	path = require('path');
 const { json } = require('express');
 
 // Initialization
 const app = express();
-require('./connect_db')
+require('./connect_db');
 
 // Settings
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 3000);
 
 // Middlewares
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(cors())
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 
 // Rest API
-app.use('/api/books', require('./rest_api/rest_api.js'))
+app.use('/api/books', require('./rest_api/rest_api.js'));
 
 // Static files
 
 // Start the server
 app.listen(app.get('port'), () => {
-    console.log('Server on port', app.get('port'));
-})
+	console.log('Server on port', app.get('port'));
+});
--- backend/rest_api/rest_api.js
+++ backend/rest_api/rest_api.js
@@ -1,10 +1,10 @@
 const Book = require('../models/Book'),
-    { Router } = require('express'),
-    router = Router();
+	{ Router } = require('express'),
+	router = Router();
 
 router.get('/', async (req, res) => {
-    const books = await Book.find()
-    res.json(books);
-})
+	const books = await Book.find();
+	res.json(books);
+});
 
-module.exports = router
+module.exports = router;
