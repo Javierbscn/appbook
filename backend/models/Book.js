9f4694caa5a0ef63f580215e19f2d0e0b1c6e420
JavierBoscan <javierboscan16@gmail.com>
2020-12-20 23:19:54 -0400

    corrigiendo fallo al guardar la fecha de creacion en Book.js

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -2,7 +2,7 @@ const { Schema, model } = require('mongoose')
 
 const BookSchema = new Schema({
     author: { type: String, required: true },
-    created_at: { type: Date, default: Date.now() },
+    created_at: { type: Date, default: Date.now },
     isbn: { type: String, required: true },
     image_path: { type: String },
     public_id: { type: String },
