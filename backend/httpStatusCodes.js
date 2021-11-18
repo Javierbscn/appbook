3158e0fcc4f1c54429cc8f1873738afce9909a53
_moliNasil <41929823+nahuelmol@users.noreply.github.com>
2021-11-18 16:29:52 -0300

    Create httpStatusCodes.js

---
+++ backend/httpStatusCodes.js
@@ -0,0 +1,12 @@
+const httpsStatusCodes = {
+	OK:200,
+	BAD_REQUEST:400,
+        UNAUTHORIZED:401,
+        FORBIDDEN:403,
+	NOT_FOUND:404,
+	INTERNAL_SERVER:500,
+        BAD_GATEWAY:502,
+        GATEWAY_TIMEOUT:504
+}
+
+module.exports = httpsStatusCodes;
